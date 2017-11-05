4281aa8582b3a6b80a559beae39b225237286183
rsanchezferrer <virgo_stepher200@hotmail.com>
2017-11-05 17:44:05 -0500

    SEGUNDO COMMIT DE CONFIGURACION

--- 3.1.Entidades/src/entidad/Ent_Alumno.java
+++ 3.1.Entidades/src/entidad/Ent_Alumno.java
@@ -2,7 +2,4 @@ package entidad;
 
 public class Ent_Alumno {
 
-	//ya, date cuenta que tu proyecto se puso con signo de interrogacion lo que significa
-	//que tienes algun cambio por enviar al git y si te das cuenta abajo hay dos cambios
-	//mandalos al git ahora
 }
